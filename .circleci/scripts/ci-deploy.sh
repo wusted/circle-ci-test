8c3000abc992729802e4b5da9039738ce9ac6225
Jean Pereira <jeankpayales444@gmail.com>
2022-12-27 20:44:23 -0600

    Automates Deployment of Kubernetes Manifests

--- .circleci/scripts/ci-deploy.sh
+++ .circleci/scripts/ci-deploy.sh
@@ -11,8 +11,8 @@ export COMMIT_SHA1=$COMMIT_SHA1
 # In-place substitution is not feasible.
 # This saves the output to be substituted into another file. 
 # Then overwrites the original file with the new one.
-envsubst < ./.circleci/kube/deployment.yaml.template > ./.circleci/kube/deployment.yaml
-echo "$KUBERNETES_KUBECONFIG" | base64 --decode > kubeconfig.yaml
+envsubst < ./.circleci/kube/deployment.yml.template > ./.circleci/kube/deployment.yml
+echo "$KUBERNETES_KUBECONFIG" | base64 --decode > kubeconfig.yml
 
-./kubectl --kubeconfig=kubeconfig.yaml get nodes
-./kubectl --kubeconfig=kubeconfig.yaml apply -f ./.circleci/kube
+./kubectl --kubeconfig=kubeconfig.yml get nodes
+./kubectl --kubeconfig=kubeconfig.yml apply -f ./.circleci/kube
